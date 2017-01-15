bff938962bc6e82b2f1e3c129d9d3e94a89ba8cf
lehevi <ic.lehevi@gmail.com>
2017-01-14 21:08:49 -0800

    Simple changes to start ctrl

---
+++ imports/components/login/login.js
@@ -0,0 +1,24 @@
+import angular from 'angular';
+import angularMeteor from 'angular-meteor';
+import template from './login.html';
+
+
+class LoginCtrl {
+  constructor() {
+    this.tasks = [{
+      text: 'This is task 1'
+    }, {
+      text: 'This is task 2'
+    }, {
+      text: 'This is task 3'
+    }];
+  }
+}
+
+export default angular.module('login', [
+  angularMeteor
+])
+  .component('login', {
+    templateUrl: 'imports/components/login/login.html',
+    controller: LoginCtrl
+  });
