6ef67e31b31f12c754e70b2fc4dda2986cd40589
lehevi <ic.lehevi@gmail.com>
2017-01-22 18:43:57 -0800

    Added update feature in Detail page.

--- imports/ui/components/accountDetail/accountDetail.js
+++ imports/ui/components/accountDetail/accountDetail.js
@@ -3,11 +3,60 @@ import angularMeteor from 'angular-meteor';
 import uiRouter from 'angular-ui-router';
 
 import template from './accountDetail.html';
+import {Accounts} from '../../../api/accounts.js';
 
 class AccountDetail{
-	constructor($stateParams){
+	constructor($stateParams, $scope, $reactive){
 		'ngInject';
-		 this.accountId = $stateParams.accountId;
+		$reactive(this).attach($scope);
+		this.accountId = $stateParams.accountId;
+
+		this.helpers({
+			// account(){
+		 //      return Accounts.findOne( {
+		 //          _id: $stateParams.accountId
+		 //        });
+			// },
+			accounts() {
+              return Accounts.find({});
+          	}
+          	, 
+          	account(){
+          		var test = Accounts.find({});
+          		var idB = $stateParams.accountId;
+				var res = idB.split('"');
+          		var title;
+          		var ID; 
+          		test.forEach(function(element) {
+          	
+          			if(element._id == res[1]){
+          				title = element.title;
+          				ID = element._id;
+
+          				console.log("one " + typeof element._id);
+          				console.log("Two " + typeof res[1]);
+          			}
+				}); 
+				return Accounts.findOne({ _id: ID });
+          	}
+		});
+	}
+	save(){
+
+		Accounts.update({
+			_id: this.account._id
+		},{
+			$set: {
+				title: this.account.title,
+				amount: this.account.amount
+			}
+		},(error)=>{
+			if(error){
+				console.log("Some error with update");
+			}else{
+				console.log("Done amigo");
+			}
+		});
 	}
 }
 
